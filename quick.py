57a4796636ed3c8291ab2bc7446985ad13054d4b
unknown <seanharveyofiangga@gmail.com>
2023-02-27 22:31:13 +0800

    created the main code for quick sort

---
+++ quick.py
@@ -0,0 +1,13 @@
+def partition(list, low, high):
+    i = low
+    pivot = list[high]
+
+    for j in range(low, high):
+        if list[j] <= pivot:
+            list[i], list[j] =  list[j], list[i]
+            i += 1
+
+    list[i], list[high] = list[high] ,list[i]
+
+    return i
+
