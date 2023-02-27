291c542803907b8db90a1f0b5424495545a2ee60
unknown <seanharveyofiangga@gmail.com>
2023-02-27 22:33:20 +0800

    added the test example

--- quick.py
+++ quick.py
@@ -11,3 +11,19 @@ def partition(list, low, high):
 
     return i
 
+def quick_sort(list, low, high):
+    if low < high:
+        partition_index = partition(list, low, high)
+        quick_sort(list, low, partition_index - 1)
+        quick_sort(list, partition_index + 1, high)
+
+def main():
+    list = [95, 7, 3, 18, 130, 9, -5, 75, 27, 25, 15, 5, 11, 69, 39, 4, 8, 14]
+    quick_sort(list, 0, len(list) - 1)
+    print(list)
+
+    list = [7, 5, 8, 2]
+    quick_sort(list, 0, len(list) - 1)
+    print(list)
+
+main()
