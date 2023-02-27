a1649258150e38457800265af028e5dccbc28700
unknown <seanharveyofiangga@gmail.com>
2023-02-27 22:12:59 +0800

    added the test example

--- insertion.py
+++ insertion.py
@@ -9,3 +9,14 @@ def insertion_sort(list):
         list[j+1] = current  
 
     return list
+
+def main():
+    list = [95, 7, 3, 18, 130, 9, -5, 75, 27, 25, 15, 5, 11, 69, 39, 4, 8, 14]
+    insertion_sort(list)
+    print(list)
+
+    list = [7, 5, 8, 2]
+    insertion_sort(list)
+    print(list)
+
+main()
