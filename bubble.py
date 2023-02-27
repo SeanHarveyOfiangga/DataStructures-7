0a68c89431b59b89960477a38abdf4a2b8327be6
unknown <seanharveyofiangga@gmail.com>
2023-02-27 21:52:05 +0800

    added the test examples

--- bubble.py
+++ bubble.py
@@ -9,3 +9,14 @@ def bubble_sort(list):
                 list[j+1] = tmp
 
     return list
+
+def main():
+    list = [95, 7, 3, 18, 130, 9, -5, 75, 27, 25, 15, 5, 11, 69, 39, 4, 8, 14]
+    bubble_sort(list)
+    print(list)
+
+    list = [7, 5, 8, 2]
+    bubble_sort(list)
+    print(list)
+
+main()
