a11d09101ac7916ed3000b5af4fb9a5050390f68
unknown <seanharveyofiangga@gmail.com>
2023-02-27 22:19:20 +0800

    created the main code for merge sort

---
+++ merge.py
@@ -0,0 +1,29 @@
+def merge_sort(list):
+    length = len(list)
+
+    if length == 1:
+        return list
+
+    mid = length // 2
+
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    output = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            output.append(left[i])
+            i += 1
+        else:
+            output.append(right[j])
+            j += 1
+
+    output.extend(left[i:])
+    output.extend(right[j:])
+
+    return output
